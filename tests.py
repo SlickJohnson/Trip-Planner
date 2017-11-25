059a8212bbb6a021cc4feaefe3825a4d4113fdca
SlickJohnson <liwa.johnson@gmail.com>
2017-11-25 14:21:41 -0800

    Implement tests for User requests

--- tests.py
+++ tests.py
@@ -1,33 +1,79 @@
+"""Unit tests for server User & Trip requests."""
 import server
 import unittest
 import json
-import bcrypt
+# import bcrypt
 import base64
 from pymongo import MongoClient
 
 
 class TripPlannerTestCase(unittest.TestCase):
+    """TripPlanner testing."""
+
     def setUp(self):
+        """Prepare server and database for testing."""
+        self.app = server.app.test_client()
 
-      self.app = server.app.test_client()
-      # Run app in testing mode to retrieve exceptions and stack traces
-      server.app.config['TESTING'] = True
+        server.app.config['TESTING'] = True
 
-      mongo = MongoClient('localhost', 27017)
-      global db
+        mongo = MongoClient('localhost', 27017)
+        global db
 
-      # Reduce encryption workloads for tests
-      server.app.bcrypt_rounds = 4
+        server.app.bcrypt_rounds = 4
 
-      db = mongo.trip_planner_test
-      server.app.db = db
+        db = mongo.trip_planner_test
+        server.app.db = db
 
-      db.drop_collection('users')
-      db.drop_collection('trips')
+        db.drop_collection('users')
 
-    # User tests, fill with test methods
-    def testCreateUser(self):
-        pass
+    def testGetUser(self):
+        """Test GET request for user."""
+        self.app.post('/user',
+                      headers=None,
+                      data=json.dumps(dict(
+                          name='Eliel Gordon',
+                          email='eliel@example.com'
+                        )),
+                      content_type='application/json')
+
+        response = self.app.get('/user',
+                                query_string=dict(email='eliel@example.com'))
+
+        json_response = json.loads(response.data.decode())
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json_response['email'], 'eliel@example.com')
+
+    def testPostUser(self):
+        """Test POST request for user."""
+        response = self.app.post('/user',
+                                 headers=None,
+                                 data=json.dumps(dict(
+                                    name="Egon Johnson",
+                                    email="egon@example.com"
+                                 )),
+                                 content_type='application/json')
+
+        json_response = json.loads(response.data.decode())
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(json_response['email'], 'egon@example.com')
+
+    def testDeleteUser(self):
+        """Test DEL request for user."""
+        self.app.post('/user',
+                      headers=None,
+                      data=json.dumps(dict(
+                          name="Delete Me",
+                          email="del@example.com"
+                        )),
+                      content_type='application/json')
+
+        response = self.app.delete('/user',
+                                   query_string=dict(email="del@example.com"))
+
+        self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
